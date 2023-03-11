2a626ccfa30b548963359b0288c92f7e9644783e
Ingus Jansons <ingusjan@gmail.com>
2023-03-11 22:29:39 +0000

    feat: add limit prompt for sets

--- index.js
+++ index.js
@@ -1,45 +1,68 @@
 #!/usr/bin/env node
 
 import { createSpinner } from "nanospinner";
-import { fetchAllEmotesFromSet } from "./lib/7tv.js";
+import { fetchAllEmotesFromSet, fetchSingularEmote } from "./lib/7tv.js";
 import { onWelcome } from "./lib/generic.js";
 import { convertAllEmotesAndSaveToDownloads } from "./lib/image-processing.js";
-import { promptForEmoteSetId, promptForEmotesToImport } from "./lib/prompts.js";
+import {
+  promptForEmoteCountLimit,
+  promptForEmoteId,
+  promptForEmoteOrEmoteSet,
+  promptForEmoteSetId,
+  promptForEmotesToImport,
+} from "./lib/prompts.js";
 
-const config = {
-  defaultEmoteSet: "61a91d43e9684edbbc38007c",
-  emoteCountLimit: 9999,
-};
+let emoteId = "";
+let emoteSetId = "";
+let emoteCountLimit = 9999;
 
-let emoteSetId = config.defaultEmoteSet;
-
-// Welcome
+// Welcome messaging
 console.clear();
 await onWelcome();
 
-// If the user hasn't set a default emote set, prompt them for one
-if (!config.defaultEmoteSet) {
+const emoteOrEmoteSet = await promptForEmoteOrEmoteSet();
+
+if (emoteOrEmoteSet === "emote") {
+  emoteId = await promptForEmoteId();
+  const spinner = createSpinner(`Fetching emote...`).start();
+  const emote = await fetchSingularEmote(emoteId);
+
+  if (!emote) {
+    spinner.error({
+      text: "Failed to fetch emote. Restart the script to try again.",
+    });
+    process.exit(1);
+  }
+
+  spinner.success({
+    text: `Fetched ${emote.name} (${emote.url}).`,
+  });
+
+  await convertAllEmotesAndSaveToDownloads([emote]);
+} else {
   emoteSetId = await promptForEmoteSetId();
+  emoteCountLimit = await promptForEmoteCountLimit();
+
+  // Fetch the emotes from a 7TV emote set
+  const spinner = createSpinner(
+    `Fetching your emotes (Set: ${emoteSetId})...`
+  ).start();
+
+  const { emotes, emoteSetName } = await fetchAllEmotesFromSet(
+    emoteSetId,
+    emoteCountLimit
+  );
+
+  if (!emotes || emotes.length < 1) {
+    spinner.error({
+      text: "Failed to fetch emotes. Restart the script to try again.",
+    });
+    process.exit(1);
+  }
+  spinner.success({
+    text: `Fetched ${emotes.length} emotes from ${emoteSetName}.`,
+  });
+
+  const emotesToImport = await promptForEmotesToImport(emotes);
+  await convertAllEmotesAndSaveToDownloads(emotesToImport);
 }
-
-// Fetch the emotes from 7TV
-const spinner = createSpinner(
-  `Fetching your emotes (Set: ${emoteSetId})...`
-).start();
-
-const { emotes, emoteSetName } = await fetchAllEmotesFromSet(
-  emoteSetId,
-  config.emoteCountLimit
-);
-
-if (!emotes) {
-  spinner.error("Failed to fetch emotes. Start the script to try again.");
-  process.exit(1);
-}
-spinner.success({
-  text: `Fetched ${emotes.length} emotes from ${emoteSetName}.`,
-});
-
-const emotesToImport = await promptForEmotesToImport(emotes);
-// console.log(emotesToImport);
-await convertAllEmotesAndSaveToDownloads(emotesToImport);
